015c6609481ba64771824339344d1c354a42550b
Jack Rowlingson <jrowlingson@esri.com>
2015-10-27 21:41:08 -0400

    teak hooks

--- index.js
+++ index.js
@@ -8,64 +8,37 @@ module.exports = {
   name: 'ember-cli-deploy-github',
 
   createDeployPlugin: function(options) {
-    //var DeployPlugin = DeployPluginBase.extend({
-    //  name: options.name,
-    //  requiredConfig: [], // TODO: jrowlingson - required config
-    //  defaultConfig: {
-    //    willDeploy: function(/* context */) {
-    //      return this._executeGithubHook('willDeploy');
-    //    },
-    //
-    //    willBuild: function(/* context */) {
-    //      return this._executeGithubHook('willBuild');
-    //    },
-    //
-    //    build: function(/* context */) {
-    //      return this._executeGithubHook('build');
-    //    },
-    //
-    //    didBuild: function(/* context */) {
-    //      return this._executeGithubHook('didBuild');
-    //    },
-    //
-    //    willUpload: function(/* context */) {
-    //      return this._executeGithubHook('willUpload');
-    //    },
-    //
-    //    upload: function(/* context */) {
-    //      return this._executeGithubHook('upload');
-    //    },
-    //
-    //    didUpload: function(/* context */) {
-    //      return this._executeGithubHook('didUpload');
-    //    },
-    //
-    //    willActivate: function(/* context */) {
-    //      return this._executeGithubHook('willActivate');
-    //    },
-    //
-    //    activate: function(/* context */) {
-    //      return this._executeGithubHook('activate');
-    //    },
-    //
-    //    didActivate: function(/* context */) {
-    //      return this._executeGithubHook('didActivate');
-    //    },
-    //
-    //    didDeploy: function(/* context */) {
-    //      return this._executeGithubHook('didDeploy');
-    //    },
-    //
-    //    didFail: function(/* context */) {
-    //      return this._executeGithubHook('didFail');
-    //    },
-    //
-    //    _executeGithubHook: function(hookName) {
-    //      // no-op
-    //      // TODO: jrowlingson - implement
-    //    }
-    //  }
-    //});
-    //return new DeployPlugin();
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      requiredConfig: [], // TODO: jrowlingson - required config
+      defaultConfig: {
+        willDeploy: function(/* context */) {
+          return this._executeGithubHook('willDeploy');
+        },
+
+        willBuild: function(/* context */) {
+          return this._executeGithubHook('willBuild');
+        },
+
+        build: function(/* context */) {
+          return this._executeGithubHook('build');
+        },
+
+        didDeploy: function(/* context */) {
+          return this._executeGithubHook('didDeploy');
+        },
+
+        didFail: function(/* context */) {
+          return this._executeGithubHook('didFail');
+        },
+
+        _executeGithubHook: function(hookName) {
+          // no-op
+          // TODO: jrowlingson - implement
+          this.log('excuting hook' + hookName);
+        }
+      }
+    });
+    return new DeployPlugin();
   }
 };
